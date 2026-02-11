9fe05a6b1a665ecacb6c6fdba83f1e36270acf6f
julienmb3-dot <julienmb3@gmail.com>
2026-02-11 19:12:59 +0100

    login & signp

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, session, redirect
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
@@ -11,19 +11,57 @@ MONGO_URI = os.getenv("MONGO_URI")
 client = MongoClient(MONGO_URI)
 db = client.get_database("Main")
 
-app = Flask(__name__)
+app = Flask("Spoon Box Rush")
+
+app.secret_key = "lkna565s6c%!dhoahjd_d"
 
 @app.route("/")
 def index():
     data_find = list(db["data"].find())
     return render_template("index.html", data=data_find)
 
-@app.route("/login")
+@app.route("/login", methods = ["POST", "GET"])
 def login():
-    return render_template("login.html")
-
-@app.route("/signup")
+    if request.method == "POST":
+        db_users = db["Users"]
+        user = db_users.find_one({"user_id" : request.form["user_id"]})
+        if user:
+            if request.form["password"] == user["password"]:
+                session["user"] = request.form["user_id"]
+                return redirect(url_for("index"))
+            else:
+                return render_template('login.html', erreur="On dirait que t'as un trou de mémoire 🕳️...")
+        else:
+            return render_template('login.html', erreur="Je ne te connais pas 😖, tu devrais t'enregistrer 😉 !")
+    else:
+        return render_template('login.html')
+    
+@app.route("/signup", methods = ["POST", "GET"])
 def signup():
-    return render_template("signup.html")
+    if request.method == "POST":
+        db_users = db["Users"]
+        new_user = db_users.find_one({"user_id" : request.form["user_id"]})
+        if new_user:
+            return render_template('signup.html', erreur="Ce nom d'utilisateur est déjà pris.")
+        else:
+            if request.form["password"] == request.form["password_validation"]:
+                if not(request.form["password"] == "" or request.form["user_id"] == ""):
+                    db_users.insert_one({
+                        "user_id" : request.form["user_id"],
+                        "password" : request.form["password"]
+                    })
+                    session["user"] = request.form["user_id"]
+                    return redirect(url_for("index"))
+                else:
+                    return render_template('signup.html', erreur="Le nom d'utilisateur ou le mot de passe est vide.")
+            else:
+                return render_template('signup.html', erreur="Les deux mots de passe ne correspondent pas.")
+    else:
+        return render_template("signup.html")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
 
 app.run(host="0.0.0.0", port=81)
